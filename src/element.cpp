da00a93b0df2ef5edea462c430d675bebe061403
Marek Hückmann <marek.hueckmann@gmail.com>
2017-05-15 13:28:40 +0200

    Code cleanup

--- src/element.cpp
+++ src/element.cpp
@@ -4,33 +4,33 @@ element::element(node left, node right, double S, double k, double alfa, double 
 	this->left = left;
 	this->right = right;
 
-    L = right.x - left.x;
+	L = right.x - left.x;
 	C = S*k/L;
 
 	// left
-
 	if (left.BC == 0) {
 		HL[0][0] =  C;
 		HL[0][1] = -C;
 		PL[0] =     0;
 	}
+
 	else if (left.BC == 1) {
 		HL[0][0] =  C;
 		HL[0][1] = -C;
-		PL[0] =     q *S;
+		PL[0] =     q*S;
 	}
-    else if (left.BC == 2) {
+
+	else if (left.BC == 2) {
 		HL[0][0] =   C + (alfa*S);
 		HL[0][1] =  -C;
 		PL[0] =     -aTemp*alfa*S;
 	}
 
 	// right
-
 	if (right.BC == 0) {
-		HL[1][0] = -(C);
-		HL[1][1] = C;
-		PL[1] = 0;
+		HL[1][0] = -C;
+		HL[1][1] =  C;
+		PL[1] =	    0;
 	}
 
 	else if (right.BC == 1) {
