b4febf5cba9471c85264c6bc8b6a13d02812a191
Marek Hückmann <marek.hueckmann@gmail.com>
2017-05-15 13:35:38 +0200

    Code cleanup

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,42 +1,42 @@
 #include "mesh.h"
 
 mesh::mesh(int ne, int nh, element *e) {
-
 	this->nh = nh;
 	this->HG = new double *[nh];
-    for (int i = 0; i < nh; i++)
+	
+	for (int i = 0; i < nh; i++)
 		HG[i] = new double[nh];
 
 	this->T = new double[nh];
-    this->PG = new double[nh];
-
-
+	this->PG = new double[nh];
+	
 	for (int i = 0; i <nh; i++) {
 		for (int j = 0; j < nh; j++)
-				HG[i][j] = 0;
+			HG[i][j] = 0;
 	}
 
 	for (int i = 0; i <nh; i++) {
-			T[i] = 0;
+		T[i] = 0;
 	}
 
 	for (int i = 0; i <nh; i++) {
-			PG[i] = 0;
+		PG[i] = 0;
 	}
 
-	// Merging local matrixes into global ones
-
+	// Merging local matrixes into global one
 	for (int i = 0; i < ne; i++) {
 		PG[(e[i].left.ID) - 1] += e[i].PL[0];
 		PG[(e[i].right.ID) - 1] += e[i].PL[1];
 
 		HG[(e[i].left.ID) - 1][(e[i].left.ID) - 1] += e[i].HL[0][0];
-        HG[(e[i].left.ID) - 1][(e[i].right.ID) - 1] += e[i].HL[0][1];
+        	HG[(e[i].left.ID) - 1][(e[i].right.ID) - 1] += e[i].HL[0][1];
 		HG[(e[i].right.ID) - 1][(e[i].left.ID) - 1] += e[i].HL[1][0];
 		HG[(e[i].right.ID) - 1][(e[i].right.ID) - 1] += e[i].HL[1][1];
 	}
 }
-void mesh::showUR() {
+
+
+void mesh::showME() {
 	cout << "\nMatrix equation:\n";
 
 	for(int i = 0; i < nh; i++) {
@@ -45,11 +45,11 @@ void mesh::showUR() {
 			cout << HG[i][j]<<" ";
 		}
 		cout << "|\tt" << i+1 << " +  ";
-
 		cout << "\t|" << PG[i] << "|\n";
 	}
 }
 
+// Gaussian elimination
 void mesh::gaussJordan() {
 	double **a = new double*[nh];
 	for (int i = 0; i < nh; i++) {
@@ -65,36 +65,32 @@ void mesh::gaussJordan() {
 	}
 
 	for (int i=0;i<nh;i++)
-        for (int k=i+1;k<nh;k++)
-            if (a[i][i]<a[k][i])
-                for (int j=0;j<=nh;j++)
-                {
-                    double temp=a[i][j];
-                    a[i][j]=a[k][j];
-                    a[k][j]=temp;
-                }
+		for (int k=i+1;k<nh;k++)
+            		if (a[i][i]<a[k][i])
+                		for (int j=0;j<=nh;j++) {
+                    			double temp=a[i][j];
+                    			a[i][j]=a[k][j];
+                    			a[k][j]=temp;
+                		}
 
-    for (int i=0;i<nh-1;i++)
-        for (int k=i+1;k<nh;k++)
-            {
-                double t=a[k][i]/a[i][i];
-                for (int j=0;j<=nh;j++)
-                    a[k][j]=a[k][j]-t*a[i][j];
-            }
+	for (int i=0;i<nh-1;i++)
+        	for (int k=i+1;k<nh;k++) {
+			double t=a[k][i]/a[i][i];
+			for (int j=0;j<=nh;j++)
+				a[k][j]=a[k][j]-t*a[i][j];
+		}
 
-    for (int i=nh-1;i>=0;i--)
-    {
-        T[i]=a[i][nh];
-        for (int j=0;j<nh;j++)
-            if (j!=i)
-                T[i]=T[i]-a[i][j]*T[j];
-        T[i]=T[i]/a[i][i];
-    }
+	for (int i=nh-1;i>=0;i--) {
+		T[i]=a[i][nh];
+		for (int j=0;j<nh;j++)
+            		if (j!=i)
+                		T[i]=T[i]-a[i][j]*T[j];
+        	T[i]=T[i]/a[i][i];
+	}
 }
 
 void mesh::showResults() {
     cout << "\nResults:\n";
-	for (int i = 0; i < nh; i++) {
+	for (int i = 0; i < nh; i++)
 		cout << "t" << i+1 << " = " << T[i]<< "\n";
-	}
 }
